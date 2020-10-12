2fa1dcec91a5818724dc7ca3f7786ecd8b18cc88
vladislavste <39482931+vladislavste@users.noreply.github.com>
2020-10-13 00:50:34 +0500

    v1.3.5 port added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from settings.application import create_application
+import os
 
 app = create_application()
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
