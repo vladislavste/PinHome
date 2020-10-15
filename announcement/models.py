165ef39438da340b4662101ed5ba807150fb76fa
vladislavste <39482931+vladislavste@users.noreply.github.com>
2020-10-15 22:09:52 +0500

    v1.3.6

--- announcement/models.py
+++ announcement/models.py
@@ -1,8 +1,3 @@
-import datetime
-import uuid
-
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy import DateTime
 from sqlalchemy.sql import func
 
@@ -23,7 +18,7 @@ class Announcement(db.Model):
     category = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
     created = db.Column(DateTime(timezone=True), server_default=func.now())
     deleted = db.Column(db.Boolean, default=False)
-    user = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
 class ImagesAnnoun(db.Model):
@@ -38,7 +33,7 @@ class Want(db.Model):
     name = db.Column(db.String(100), nullable=False)
     announcement = db.Column(db.Integer, db.ForeignKey('announcement.id'), nullable=False)
     created = db.Column(DateTime(timezone=True), server_default=func.now())
-    user = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
 class Deal(db.Model):
--- announcement/schema.py
+++ announcement/schema.py
@@ -1,6 +1,6 @@
 from marshmallow_sqlalchemy import ModelSchema
 from marshmallow import fields
-from .models import Announcement, ImagesAnnoun
+from .models import Announcement, ImagesAnnoun, Category
 from ext import db
 
 
@@ -8,7 +8,8 @@ class AnnouncementSchema(ModelSchema):
     class Meta(ModelSchema.Meta):
         model = Announcement
         sqla_session = db.session
-    id = fields.Number(dump_only=True)
+
+    id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
     description = fields.String(required=True)
     saled = fields.Boolean(required=False)
@@ -21,7 +22,18 @@ class AnnouncementImageSchema(ModelSchema):
     class Meta(ModelSchema.Meta):
         model = ImagesAnnoun
         sqla_session = db.session
-    id = fields.Number(dump_only=True)
+
+    id = fields.Integer(dump_only=True)
     image_path = fields.String(required=True)
     created = fields.Date(dump_only=True)
-    announcement = fields.Number(required=True)
+    announcement = fields.Integer(required=True)
+
+
+class CategorySchema(ModelSchema):
+    class Meta(ModelSchema.Meta):
+        model = Category
+        sqla_session = db.session
+
+    id = fields.Integer(required=False)
+    name = fields.String(required=False)
+    parent = fields.Integer(required=False)
