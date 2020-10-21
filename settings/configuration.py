05fa73d0048b964f822ef88e3279e66ed300bd7b
Dmitry Borutya <borutia@mail.ru>
2020-10-21 12:50:35 +0500

    save

--- settings/configuration.py
+++ settings/configuration.py
@@ -5,8 +5,8 @@ class Config:
     DEBUG = False
     TESTING = False
     # SQLALCHEMY_DATABASE_URI = 'postgresql://pinhouse:pinhouse@localhost:5433/pinhouse'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:2452020@localhost:5432/pinhome'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:2452020@localhost:5432/pinhome'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
     PROJECT_HOME = os.path.dirname(os.path.realpath(__file__)).replace('/settings', '')
