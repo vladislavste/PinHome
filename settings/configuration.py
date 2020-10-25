983e6d50db40696350ef2d540966b01aa860d241
Dmitry Borutya <borutia@mail.ru>
2020-10-25 17:57:02 +0500

    save

--- settings/configuration.py
+++ settings/configuration.py
@@ -4,9 +4,9 @@ import os
 class Config:
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'postgres://kshcgzkjxlfwdc:2a105932715ec1b6732b6ab9204b252544d03aaceff10005f28512a68e30a9f4@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/d7vc1htmtmq5fa'
+    #SQLALCHEMY_DATABASE_URI = 'postgres://kshcgzkjxlfwdc:2a105932715ec1b6732b6ab9204b252544d03aaceff10005f28512a68e30a9f4@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/d7vc1htmtmq5fa'
     #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:2452020@localhost:5432/pinhome'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
     PROJECT_HOME = os.path.dirname(os.path.realpath(__file__)).replace('/settings', '')
