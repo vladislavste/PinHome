c657da89d9aab6c73d1da97e364d8874b5c3a327
vladislavste <39482931+vladislavste@users.noreply.github.com>
2020-10-30 00:42:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from settings.application import create_application
+import logging
 
 app = create_application()
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger.setLevel(logging.DEBUG)
+app.logger.debug('this will show in the log')
 
 if __name__ == '__main__':
     app.run(debug=True)
