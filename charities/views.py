664a92afcfee12a98b7c06747b8cb43c342a2eda
Борутя Дмитрий Александрович <u8389@hades.uralairlines>
2020-11-27 09:32:24 +0500

    save

--- charities/views.py
+++ charities/views.py
@@ -38,126 +38,126 @@ def one_charities(token, id):
 @token_check
 def create_charities(token):
     get_user = User.query.filter(User.token == token).one()
-    if get_user.username == 'Admin' or get_user.username == 'admin':
-        try:
-            data_charities = json.loads(request.form['charities'])
-            check_charities = db.session.query(Charities).filter_by(name=data_charities['name']).first()
-            if check_charities:
-                return {'error': 'Charities is already registered'}, 200
-            сharities_schema = Charities_schema()
-            charities = сharities_schema.load(data_charities)
-            db.session.add(charities)
-            db.session.flush()
-            id = charities.id
+    #if get_user.username == 'Admin' or get_user.username == 'admin':
+    try:
+        data_charities = json.loads(request.form['charities'])
+        check_charities = db.session.query(Charities).filter_by(name=data_charities['name']).first()
+        if check_charities:
+            return {'error': 'Charities is already registered'}, 200
+        сharities_schema = Charities_schema()
+        charities = сharities_schema.load(data_charities)
+        db.session.add(charities)
+        db.session.flush()
+        id = charities.id
 
-            data_for_charities_address = json.loads(request.form['charities_address'])
-            charities_address_schema = Charities_address_schema()
-            for val in data_for_charities_address['address']:
-                data_charities_address = {
-                    'address': val,
-                    'id_charities': id
-                }
-                charities_address = charities_address_schema.load(data_charities_address)
-                db.session.add(charities_address)
+        data_for_charities_address = json.loads(request.form['charities_address'])
+        charities_address_schema = Charities_address_schema()
+        for val in data_for_charities_address['address']:
+            data_charities_address = {
+                'address': val,
+                'id_charities': id
+            }
+            charities_address = charities_address_schema.load(data_charities_address)
+            db.session.add(charities_address)
 
-            data_for_charities_contacts = json.loads(request.form['charities_contacts'])
-            charities_contacts_schema = Charities_contacts_schema()
-            for val in data_for_charities_contacts['contact']:
-                data_charities_contacts = {
-                    'contact': val,
-                    'id_charities': id
-                }
-                charities_contacts = charities_contacts_schema.load(data_charities_contacts)
-                db.session.add(charities_contacts)
+        data_for_charities_contacts = json.loads(request.form['charities_contacts'])
+        charities_contacts_schema = Charities_contacts_schema()
+        for val in data_for_charities_contacts['contact']:
+            data_charities_contacts = {
+                'contact': val,
+                'id_charities': id
+            }
+            charities_contacts = charities_contacts_schema.load(data_charities_contacts)
+            db.session.add(charities_contacts)
 
-            data_for_charities_social_networks = json.loads(request.form['charities_social_networks'])
-            charities_social_networks_schema = Charities_social_networks_schema()
-            for val in data_for_charities_social_networks['social_networks']:
-                data_charities_social_networks = {
-                    'social_networks': val,
-                    'id_charities': id
-                }
-                charities_social_networks = charities_social_networks_schema.load(data_charities_social_networks)
-                db.session.add(charities_social_networks)
+        data_for_charities_social_networks = json.loads(request.form['charities_social_networks'])
+        charities_social_networks_schema = Charities_social_networks_schema()
+        for val in data_for_charities_social_networks['social_networks']:
+            data_charities_social_networks = {
+                'social_networks': val,
+                'id_charities': id
+            }
+            charities_social_networks = charities_social_networks_schema.load(data_charities_social_networks)
+            db.session.add(charities_social_networks)
 
-            image_schema = Images_charities_schema()
-            if request.files.get('photo'):
-                file = request.files.get('photo')
-                if file and allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    extension = filename.split()[-1]
-                    new_filename = "upload-{}.{}".format(
-                        uuid4(), extension
-                    )
+        image_schema = Images_charities_schema()
+        if request.files.get('photo'):
+            file = request.files.get('photo')
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                extension = filename.split()[-1]
+                new_filename = "upload-{}.{}".format(
+                    uuid4(), extension
+                )
 
-                    file.save(os.path.join(current_app.config['UPLOAD_FOLDER_CHARITIES'], new_filename))
-                    img_data = {
-                        "image_path": f'/images/charities/{new_filename}',
-                        "id_charities": id
-                    }
-                    db_image = image_schema.load(img_data)
-                    db.session.add(db_image)
-            else:
+                file.save(os.path.join(current_app.config['UPLOAD_FOLDER_CHARITIES'], new_filename))
                 img_data = {
-                    "image_path": f'/images/charities.png',
+                    "image_path": f'/images/charities/{new_filename}',
                     "id_charities": id
                 }
                 db_image = image_schema.load(img_data)
                 db.session.add(db_image)
-            db.session.commit()
-            return {'message': 'successfully!'}, 201
-        except:
-            return {'error': 'Error in data'}, 400
-    return {'error': 'You not admin!'}, 401
+        else:
+            img_data = {
+                "image_path": f'/images/charities.png',
+                "id_charities": id
+            }
+            db_image = image_schema.load(img_data)
+            db.session.add(db_image)
+        db.session.commit()
+        return {'message': 'successfully!'}, 201
+    except:
+        return {'error': 'Error in data'}, 400
+   # return {'error': 'You not admin!'}, 401
 
 
 @charities.route('/<id>', methods=['PUT'])
 @token_check
 def update_charities(token, id):
     get_user = User.query.filter(User.token == token).one()
-    if get_user.username == 'Admin' or get_user.username == 'admin':
+    #if get_user.username == 'Admin' or get_user.username == 'admin':
+    try:
+        charities = Charities.query.get(id)
+    except:
+        return {'error': 'Charities not found'}, 401
+    if request.form:
+        data = json.loads(request.form['charities'])
+        data['id'] = id
+        сharities_schema = Charities_schema()
+        charities = сharities_schema.load(data=json.dumps(data), many=True)
+        db.session.add(charities)
+        db.session.commit()
         try:
-            charities = Charities.query.get(id)
+            image_schema = Images_charities_schema()
+            file = request.files.get('photo')
+            if file and allowed_file(file.filename):
+                get_photo = db.session.query(Images_charities).filter_by(id_charities=id).first()
+                if get_photo:
+                    photo = Images_charities.query.get(get_photo.id)
+                    db.session.delete(photo)
+                    if photo.image_path != '/images/default.jpg':
+                        path_delete = current_app.config['PROJECT_HOME'] + photo.image_path
+                        os.remove(path_delete)
+                    db.session.commit()
+
+                filename = secure_filename(file.filename)
+                extension = filename.split()[-1]
+                new_filename = "upload-{}.{}".format(
+                    uuid4(), extension
+                )
+
+                file.save(os.path.join(current_app.config['UPLOAD_FOLDER_CHARITIES'], new_filename))
+                img_data = {
+                    "image_path": f'/images/charities/{new_filename}',
+                    "id_charities": id
+                }
+                db_image = image_schema.load(img_data)
+                db.session.add(db_image)
         except:
-            return {'error': 'Charities not found'}, 401
-        if request.form:
-            data = json.loads(request.form['charities'])
-            data['id'] = id
-            сharities_schema = Charities_schema()
-            charities = сharities_schema.load(data=json.dumps(data), many=True)
-            db.session.add(charities)
-            db.session.commit()
-            try:
-                image_schema = Images_charities_schema()
-                file = request.files.get('photo')
-                if file and allowed_file(file.filename):
-                    get_photo = db.session.query(Images_charities).filter_by(id_charities=id).first()
-                    if get_photo:
-                        photo = Images_charities.query.get(get_photo.id)
-                        db.session.delete(photo)
-                        if photo.image_path != '/images/default.jpg':
-                            path_delete = current_app.config['PROJECT_HOME'] + photo.image_path
-                            os.remove(path_delete)
-                        db.session.commit()
-
-                    filename = secure_filename(file.filename)
-                    extension = filename.split()[-1]
-                    new_filename = "upload-{}.{}".format(
-                        uuid4(), extension
-                    )
-
-                    file.save(os.path.join(current_app.config['UPLOAD_FOLDER_CHARITIES'], new_filename))
-                    img_data = {
-                        "image_path": f'/images/charities/{new_filename}',
-                        "id_charities": id
-                    }
-                    db_image = image_schema.load(img_data)
-                    db.session.add(db_image)
-            except:
-                return {'message': 'error edit photo'}, 401
-            db.session.commit()
-            return {'message': 'successfully!'}, 201
-    return {'error': 'You not admin!'}, 401
+            return {'message': 'error edit photo'}, 401
+        db.session.commit()
+        return {'message': 'successfully!'}, 201
+   # return {'error': 'You not admin!'}, 401
 
 
 @charities.route('/<id>', methods=['DELETE'])
